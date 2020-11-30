8c2035666fbffd51dd0abb494efca2722926501a
gochase <gochase@umich.edu>
2020-11-30 14:17:44 -0500

    Checking the gitignore file.

--- database.py
+++ database.py
@@ -18,10 +18,19 @@ import os
 # for the first 100 items, grab 25 items 4 times, after the first 100 items you can put them into the db all in one go
 
 # 4. What do we do about the fact that our database is way too large to go on github? How are we supposed to fill it properly?
-# cut down on the data
+# cut down on the data (I think this is the best option for us bc the results don't really matter that much)
 # OR
 # you can compress the data in the database 
 # https://docs.github.com/en/free-pro-team@latest/github/managing-large-files/working-with-large-files
+
+# keep the kaggle data as CSV files
+
+# Ideas for reformatting the code:
+# This file, database.py, can have only functions that set up the database and set up each table inside of the database
+# Then, in the other files where we are running scripts that have info that needs to get inserted into the db, we can simply scrape
+# the info and then insert it directly into the corresponding table in the database
+# We really should put the kaggle data into the database as well just so everything is in the database
+
 
 
 def setUpDatabase(db_name):
@@ -31,25 +40,51 @@ def setUpDatabase(db_name):
     return cur, conn
 
 
-def setUpKaggleTable(cur, conn):
+def initializeKaggleTable(cur, conn):
     cur.execute("DROP TABLE IF EXISTS Kaggle")
     cur.execute("CREATE TABLE Kaggle (story_text TEXT, real_news INT)")
 
     conn.commit()
 
-def fillKaggleTable(kaggle_csv_filename, cur, conn):
-    setUpKaggleTable(cur, conn)
-    path = os.path.dirname(os.path.abspath(__file__)) + '/Kaggle/' + kaggle_csv_filename
-    try:
-        with open(path, 'r') as infile:
-            rows = infile.readlines()
-            for row in rows[1:]:
-                story_text = row[:-3]
-                real_news_indicator = row.split(',')[-1]
-                cur.execute("INSERT INTO Kaggle (story_text,real_news) VALUES (?,?)",(story_text, real_news_indicator))
-        conn.commit()
-    except:
-        print("Unable to open {}. Please try again.".format(kaggle_csv_filename))
+def initializeNewsTypesTable(curr, conn):
+    pass
+
+def initializeNewsSourcesTable(curr, conn):
+    pass
+
+def initializeTwitterUsersTable(curr, conn):
+    pass
+
+def initializeTwitterDataTable(curr, conn):
+    pass
+
+def initializeNewsAPIDataTable(curr, conn):
+    pass
+
+def initializeNYTimesTable(curr, conn):
+    pass
+
+def initializeTwitterTable(curr, conn):
+    pass
+
+def initializeWSJTable(curr, conn):
+    pass
+
+
+
+# def fillKaggleTable(kaggle_csv_filename, cur, conn):
+#     setUpKaggleTable(cur, conn)
+#     path = os.path.dirname(os.path.abspath(__file__)) + '/Kaggle/' + kaggle_csv_filename
+#     try:
+#         with open(path, 'r') as infile:
+#             rows = infile.readlines()
+#             for row in rows[1:]:
+#                 story_text = row[:-3]
+#                 real_news_indicator = row.split(',')[-1]
+#                 cur.execute("INSERT INTO Kaggle (story_text,real_news) VALUES (?,?)",(story_text, real_news_indicator))
+#         conn.commit()
+#     except:
+#         print("Unable to open {}. Please try again.".format(kaggle_csv_filename))
 
 
 
