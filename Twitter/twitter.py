c4276e53d33b80ce9e7abdbe5745063297d22ec6
gho19 <grantyho@gmail.com>
2020-11-30 13:06:37 -0700

    Twitter 25 Tweets at a time

--- Twitter/twitter.py
+++ Twitter/twitter.py
@@ -10,20 +10,28 @@ auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit = True)
 
-username = 'JoeBiden'
-count = 50000 # number of Tweets to scrape
-try:     
- # Creation of query method using parameters
- tweets = tweepy.Cursor(api.user_timeline, id = username).items(count)
- 
- # Pulling information from Tweets iterable object
- tweets_list = [['Twitter', username, tweet.text, tweet.created_at] for tweet in tweets]
- 
- # Creation of dataframe from Tweets list
- # Add or remove columns as you remove tweet information
- tweets_df = pd.DataFrame(tweets_list, columns = ['source', 'username', 'content', 'date'])
-except BaseException as e:
-      print('failed on_status,', str(e))
-      time.sleep(3)
+usernames = ['JoeBiden', 'realDonaldTrump', 'KamalaHarris', 'Mike_Pence']
+count = 25 # number of Tweets to scrape
+tweets_df = pd.DataFrame()
 
-tweets_df.to_csv('biden_twitter.csv', index = False)
+for name in usernames:
+      try:     
+            # Creation of query method using parameters
+            tweets = tweepy.Cursor(api.user_timeline, id = name).items(count)
+            
+            # Pulling information from Tweets iterable object
+            tweets_list = [['Twitter', name, tweet.text, tweet.created_at] for tweet in tweets]
+            
+            # Creation of dataframe from Tweets list
+            # Add or remove columns as you remove tweet information
+
+            tweets_df = tweets_df.append(pd.DataFrame(tweets_list))
+            
+
+      except BaseException as e:
+            print('failed on_status,', str(e))
+            time.sleep(3)
+
+tweets_df.to_csv('election2020tweets.csv', index = False)
+
+
