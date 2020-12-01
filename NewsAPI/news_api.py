c1321125bac18f707fe49fdb81ee5bd5da598064
gho19 <grantyho@gmail.com>
2020-11-30 22:53:40 -0700

    Correctly Implemented Twitter and News API DB

--- NewsAPI/news_api.py
+++ NewsAPI/news_api.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+import sqlite3
+import os
 
 # API Key: 61e924b119ab4e4294956f2da5d1d01c
 
@@ -7,15 +9,75 @@ url = ('http://newsapi.org/v2/top-headlines?country=us&apiKey=61e924b119ab4e4294
 response = requests.get(url)
 data = response.json()
 
-entries = []
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
 
-for news in data['articles']:
-    title = news['title'].split(' - ')
-    source = news['source']['name']
-    entries.append([source, title[0], news['description'], news['publishedAt']])
+def newsApiData():
+    entries = []
 
-news_df = pd.DataFrame(entries, columns = ['source', 'title', 'storyContent', 'pubDate'])
-news_df.to_csv('news_api.csv', index = False)
+    for news in data['articles']:
+        title = news['title'].split(' - ')
+        source = news['source']['name']
+        entries.append([source, title[0], news['description'], news['publishedAt'], news['url']])
+
+    return entries
+
+
+def newsApiSourcesTable(data):
+    cur.execute("CREATE TABLE IF NOT EXISTS news_api_sources (SourceId INTEGER PRIMARY KEY, Source TEXT)")
+
+    # Check how many rows in DB Table
+    cur.execute('SELECT COUNT(*) from news_api_sources')
+    cur_result = cur.fetchone()
+    rows = cur_result[0]
+
+    # Count for IDs
+    # Check how many rows in database to set TweetId
+    if rows == 0: 
+        data_count = 1
+    else: 
+        cur.execute("SELECT * FROM news_api_sources WHERE SourceId = (SELECT MAX(SourceId) FROM news_api_sources)")
+        data_count = cur.fetchone()[0] + 1     
+
+    for entry in range(len(data)):
+        cur.execute("INSERT INTO news_api_sources (SourceId, Source) VALUES (?,?)", (data_count, data[entry][0]))
+        data_count += 1
+    conn.commit()
+
+
+def newsApiTable(data):
+    cur.execute("CREATE TABLE IF NOT EXISTS news_api (ArticleId INTEGER PRIMARY KEY, Title TEXT, Description TEXT, Timestamp TEXT, Url TEXT, SourceId INTEGER, UNIQUE(Url), FOREIGN KEY (SourceId) REFERENCES news_api_sources (SourceId))")
+
+    # Check how many rows in DB Table
+    cur.execute('SELECT COUNT(*) from news_api')
+    cur_result = cur.fetchone()
+    rows = cur_result[0]
+
+    # Count for IDs
+    # Check how many rows in database to set TweetId
+    if rows == 0: 
+        data_count = 1
+    else: 
+        cur.execute("SELECT * FROM news_api WHERE ArticleId = (SELECT MAX(ArticleId) FROM news_api)")
+        data_count = cur.fetchone()[0] + 1     
+      
+    name_count = 1
+
+    for entry in range(len(data)):
+        cur.execute("INSERT INTO news_api (ArticleId, Title, Description, Timestamp, Url, SourceId) VALUES (?,?,?,?,?,?)", (data_count, data[entry][1], data[entry][2], data[entry][3], data[entry][4], name_count))
+        data_count += 1
+        name_count += 1
+    conn.commit()
+
+cur, conn = setUpDatabase('news_api.db')
+data = newsApiData()
+newsApiSourcesTable(data)
+newsApiTable(data)
+
+
 
 
 
--- Twitter/twitter.py
+++ Twitter/twitter.py
@@ -19,22 +19,18 @@ def setUpDatabase(db_name):
     return cur, conn
 
 def twitterData(user):
-      # Usernames from Twitter to scrape Tweets from 
-      usernames = ['JoeBiden', 'realDonaldTrump', 'KamalaHarris', 'Mike_Pence']
-
       # Number of Tweets to scrape per user
-      count = 25 
+      count = 25
 
       data = []
 
-      
       try:     
             # Creation of query method using parameters
-            tweets = tweepy.Cursor(api.user_timeline, id = user).items(count)
+            tweets = tweepy.Cursor(api.user_timeline, id = user, since_id = '1/1/2020').items(count)
                   
             # Pulling information from Tweets iterable object
             # tweet_list only contains 25 data points per Twitter user
-            tweets_list = [[user, tweet.text.strip(), tweet.created_at] for tweet in tweets]
+            tweets_list = [[tweet.id, tweet.text.strip(), tweet.created_at] for tweet in tweets]
             for i in range(len(tweets_list)):
                   data.append(tweets_list[i])
             
@@ -47,31 +43,45 @@ def twitterData(user):
 
 # Creates table in database with UserId and Username
 def twitterUsersTable(usernames):
-      cur.execute("DROP TABLE IF EXISTS twitter_users")
-      cur.execute("CREATE TABLE twitter_users (UserId INTEGER PRIMARY KEY, Username TEXT)")
+      cur.execute("CREATE TABLE IF NOT EXISTS twitter_users (UserId INTEGER PRIMARY KEY, Username TEXT)")
 
       for name in range(len(usernames)):
             cur.execute("INSERT INTO twitter_users (UserId, Username) VALUES (?,?)", (name + 1, usernames[name]))
+            conn.commit()
+
 
 
 # Creates table and adds data scraped from API
 def twitterTable(usernames):
-      cur.execute("CREATE TABLE IF NOT EXISTS twitter(TweetId INTEGER PRIMARY KEY, Tweet TEXT, Timestamp TEXT, UserId INTEGER, FOREIGN KEY (UserId) REFERENCES twitter_users (UserId))")
+      cur.execute("CREATE TABLE IF NOT EXISTS twitter (TweetId INTEGER PRIMARY KEY, Tweet TEXT, Timestamp TEXT, TweetNum INTEGER, UserId INTEGER, UNIQUE(TweetNum), FOREIGN KEY (UserId) REFERENCES twitter_users (UserId))")
 
-      # Count for IDs 
-      data_count = 1
+      # Check how many rows in DB Table
+      cur.execute('SELECT COUNT(*) from twitter')
+      cur_result = cur.fetchone()
+      rows = cur_result[0]
+
+      # Count for IDs
+      # Check how many rows in database to set TweetId
+      if rows == 0: 
+            data_count = 1
+      else: 
+            cur.execute("SELECT * FROM twitter WHERE TweetId = (SELECT MAX(TweetId) FROM twitter)")
+            data_count = cur.fetchone()[0] + 1     
+      
       name_count = 1
 
       for name in usernames:
             # Scrape Twitter based upon username
             data = twitterData(name)
+
             # Setup database
             for row in range(len(data)):
-                  cur.execute("INSERT INTO twitter (TweetId, UserId, Tweet, Timestamp) VALUES (?,?,?,?)", (data_count, name_count, data[row][1], data[row][2]))
+                  cur.execute("INSERT INTO twitter (TweetId, TweetNum, Tweet, Timestamp, UserId) VALUES (?,?,?,?,?)", (data_count, data[row][0], data[row][1], data[row][2], name_count,))
                   data_count += 1
             name_count += 1
             conn.commit()
             time.sleep(5)
+            
 
 # Setup DB
 cur, conn = setUpDatabase('twitter.db')
