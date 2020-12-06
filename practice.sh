f7b8f934ece20970826d3b52e37b9cf372df4da9
gochase <gochase@umich.edu>
2020-12-06 10:59:59 -0500

    updating shell script.

--- practice.sh
+++ practice.sh
@@ -1,17 +1,32 @@
-# for i in {0..10}
-# do
-#     python3 nytimes.py --numRuns $i
-#     read -t 10
-# done
-
+# Wall Street Journal
 for i in {7..11}
 do
-    for j in {1..5}
+    for j in {1..29}
     do
         python3 wsj.py --month $i --day $j
         read -t 10
     done
 done
 
+# New York Times
+for i in {0..23}
+do
+    python3 nytimes.py --numRuns $i
+    read -t 10
+done
 
-# python wsj.py --month 7 --day 1
+# News API
+for i in {0..5}
+do
+    python3 nytimes.py --numRuns $i
+    read -t 3600
+done
+
+# Twitter
+python3 twitter.py
+
+# ML Calculation
+python3 database.py
+
+# Visualizations and Calculation File
+python3 visualizations.py
